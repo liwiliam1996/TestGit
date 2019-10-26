dfbc4c51b4dfdc25834799a6c4e82ae8e03ac44c
liwilling <255893324@qq.com>
2019-10-26 17:32:20 +0800

    测试

---
+++ git-test-project/src/com/xrzh/git/FirstClass.java
@@ -0,0 +1,20 @@
+package com.xrzh.git;
+
+/**
+ * @ClassName:FirstClass
+ * @Description:测试git
+ * @Author:liwiliam
+ * @Date:2019/10/26 17:24
+ * @Version:1.0
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+}
